e9517bb8b44afa9e447e54b0223698c470dd92af
Santosh <santoshcse97@gmail.com>
2021-05-10 11:12:25 +0530

    bug fixed

--- app.py
+++ app.py
@@ -439,4 +439,4 @@ def updatepass():
         
 # Running flask App
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
